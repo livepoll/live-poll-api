3b8117a221625a1760300cb19a7c46ef429815fa
Marc Auberer <marc.auberer@chillibits.com>
2020-10-06 13:34:03 +0200

    Remove demo test to make build working

--- src/test/kotlin/de/livepoll/api/LivePollApplicationTests.kt
+++ src/test/kotlin/de/livepoll/api/LivePollApplicationTests.kt
@@ -1,13 +1,8 @@
 package de.livepoll.api
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class LivePollApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
 
 }
