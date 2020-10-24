4f66616a089c11fa418bc8e8224e620340c37332
Marc Auberer <marc.auberer@sap.com>
2020-10-24 21:25:01 +0200

    Extract mysql credentials to env variables

--- src/main/kotlin/de/livepoll/api/controller/PollController.kt
+++ src/main/kotlin/de/livepoll/api/controller/PollController.kt
@@ -1,27 +1,20 @@
 package de.livepoll.api.controller
 
 import de.livepoll.api.entity.Poll
-import de.livepoll.api.entity.User
 import de.livepoll.api.repository.PollRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import de.livepoll.api.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired
 
 @RestController
-class helloworld{
+class PollController {
 
     @Autowired
     lateinit var pollRepository: PollRepository
 
-    @GetMapping("/helloworld")
-    fun helloworld(): String{
-        return "Hello World";
-    }
-
-    @GetMapping("/getAllPolls")
+    @GetMapping("/polls")
     fun getAllPolls(): List<Poll>{
-        var pollsList: List<Poll> = pollRepository.findAll();
+        val pollsList: List<Poll> = pollRepository.findAll();
         return pollsList;
     }
 }
