180ce04b6ba7ed624b3d0c00a44d304c5e18efd7
Ordinateur-Hack <dominic-plein@gmx.de>
2020-11-09 18:48:20 +0100

    Fix scanBasePackage name

--- src/main/kotlin/de/livepoll/api/LivePollApplication.kt
+++ src/main/kotlin/de/livepoll/api/LivePollApplication.kt
@@ -3,7 +3,7 @@ package de.livepoll.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["de.live.poll.api"])
+@SpringBootApplication(scanBasePackages = ["de.livepoll.api"])
 class LivePollApplication
 
 fun main(args: Array<String>) {
