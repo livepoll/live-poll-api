806b8a9fa1dff0da713c2f5acdf51ba1411c354d
Ordinateur-Hack <dominic-plein@gmx.de>
2020-12-07 00:10:22 +0100

    Add custom delete by user query to poll repository

--- src/main/kotlin/de/livepoll/api/repository/PollRepository.kt
+++ src/main/kotlin/de/livepoll/api/repository/PollRepository.kt
@@ -1,8 +1,14 @@
 package de.livepoll.api.repository
 
 import de.livepoll.api.entity.db.Poll
+import de.livepoll.api.entity.db.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
-interface PollRepository: JpaRepository<Poll, Int>
+interface PollRepository : JpaRepository<Poll, Int> {
+    // https://stackoverflow.com/a/37491875/9655481
+    @Transactional
+    fun deleteByUser(user: User)
+}
