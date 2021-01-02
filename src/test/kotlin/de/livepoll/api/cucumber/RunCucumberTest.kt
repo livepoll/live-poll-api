a87c1e9ab9fb70ff08945a2c8a7dca050ed64d5c
Ordinateur-Hack <dominic-plein@gmx.de>
2021-01-02 13:18:00 +0100

    Rename Cucumber test runner

---
+++ src/test/kotlin/de/livepoll/api/cucumber/RunCucumberTest.kt
@@ -0,0 +1,13 @@
+package de.livepoll.api.cucumber
+
+import io.cucumber.junit.Cucumber
+import io.cucumber.junit.CucumberOptions
+import org.junit.runner.RunWith
+
+@RunWith(Cucumber::class)
+@CucumberOptions(
+    features = ["src/test/resources/features/"],
+    plugin = ["pretty", "html:target/cucumber-report.html"]
+)
+class RunCucumberTest {
+}
