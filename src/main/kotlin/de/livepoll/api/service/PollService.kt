ff09025d196414b16c70903851a7fad866bc546c
Ordinateur-Hack <dominic-plein@gmx.de>
2021-04-26 22:28:15 +0200

    Implement new inheritance strategy for poll items

--- src/main/kotlin/de/livepoll/api/service/PollService.kt
+++ src/main/kotlin/de/livepoll/api/service/PollService.kt
@@ -1,109 +1,89 @@
 package de.livepoll.api.service
 
-import de.livepoll.api.entity.db.*
-import de.livepoll.api.entity.dto.MultipleChoiceItemDtoIn
+import de.livepoll.api.entity.db.Poll
+import de.livepoll.api.entity.db.PollItem
 import de.livepoll.api.entity.dto.PollDtoIn
 import de.livepoll.api.entity.dto.PollDtoOut
-import de.livepoll.api.entity.dto.QuizItemDtoIn
-import de.livepoll.api.repository.*
+import de.livepoll.api.entity.dto.PollItemDtoOut
+import de.livepoll.api.repository.PollRepository
+import de.livepoll.api.repository.UserRepository
 import de.livepoll.api.util.toDtoOut
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
 
 @Service
 class PollService(
-        private val userRepository: UserRepository,
-        private val pollRepository: PollRepository,
-        private val multipleChoiceItemRepository: MultipleChoiceItemRepository,
-        private val answerRepository: AnswerRepository,
-        private val quizItemRepository: QuizItemRepository,
-        private val openTextItemRepository: OpenTextItemRepository
+    private val userRepository: UserRepository,
+    private val pollRepository: PollRepository,
+    private val pollItemService: PollItemService
 ) {
 
-    fun createPollEntity(pollDto: PollDtoIn, userId: Int) {
-        userRepository.findById(userId).orElseGet { null }.run {
-            val poll = Poll(0, this, pollDto.name, pollDto.startDate, pollDto.endDate, java.util.UUID.randomUUID().toString(), null ,emptyList<PollItem>().toMutableList())
-            pollRepository.saveAndFlush(poll)
-        }
+    //--------------------------------------------- Get ----------------------------------------------------------------
+    fun getPoll(pollId: Long): PollDtoOut {
+        return pollRepository.findById(pollId)
+            .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND, "Poll not found") }
+            .run { this.toDtoOut() }
     }
 
-    fun createMultipleChoiceItem(item: MultipleChoiceItemDtoIn): MultipleChoiceItem {
-        pollRepository.findById(item.pollId).orElseGet { null }.run {
-            val multipleChoiceItem = MultipleChoiceItem(0, this, item.question, item.position, emptyList())
-            val answers = item.answers.map { Answer(0, multipleChoiceItem, it) }
-            multipleChoiceItemRepository.saveAndFlush(multipleChoiceItem)
-            answerRepository.saveAll(answers)
-            return multipleChoiceItem
-        }
-    }
-
-    fun createQuizItem(item: QuizItemDtoIn): QuizItem {
-        pollRepository.findById(item.pollId).orElseGet { null }.run {
-            val quizItem = QuizItem(0, this, item.question, item.position, emptyList())
-            val answers = item.answers.map { Answer(0, quizItem, it) }
-            quizItemRepository.saveAndFlush(quizItem)
-            answerRepository.saveAll(answers)
-            return quizItem
-        }
-    }
-
-    fun getPoll(id: Int): ResponseEntity<*> {
-        pollRepository.findById(id).orElseGet {
-            throw ResponseStatusException(HttpStatus.NOT_FOUND, "This poll does not exist")
-        }.run {
-            return ResponseEntity.ok().body(this.toDtoOut())
-        }
-    }
-
-    fun getPollItemsForPoll(pollId: Int): ResponseEntity<*> {
-        pollRepository.findById(pollId).orElseGet {
-            throw ResponseStatusException(HttpStatus.NOT_FOUND, "This poll does not exist")
-        }.run {
-            val pollItemsOut = this.pollItems.map { it.toDtoOut() }
-            return ResponseEntity.ok().body(pollItemsOut)
-        }
-    }
-
-    fun deletePoll(id: Int) {
-        pollRepository.findById(id).orElseGet { null }.run {
-            if (this.user.username == SecurityContextHolder.getContext().authentication.name) {
-                try {
-                    pollRepository.deleteById(id)
-                } catch (ex: EmptyResultDataAccessException) {
-                }
-                try {
-                    multipleChoiceItemRepository.deleteById(id)
-                } catch (ex: EmptyResultDataAccessException) {
-                }
-                try {
-                    quizItemRepository.deleteById(id)
-                } catch (ex: EmptyResultDataAccessException) {
-                }
-                try {
-                    openTextItemRepository.deleteById(id)
-                } catch (ex: EmptyResultDataAccessException) {
-                }
+    fun getPollItemsForPoll(pollId: Long): List<PollItemDtoOut> {
+        pollRepository.findById(pollId)
+            .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND, "Poll not found") }
+            .run {
+                return this.pollItems.map { pollItemService.getPollItem(it.id) }
             }
+    }
+
+
+    //-------------------------------------------- Create --------------------------------------------------------------
+
+    fun createPoll(pollDto: PollDtoIn, userId: Long): PollDtoOut {
+        userRepository.findById(userId)
+            .orElseThrow {
+                ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")
+            }
+            .run {
+                val poll = Poll(
+                    0,
+                    this,
+                    pollDto.name,
+                    pollDto.startDate,
+                    pollDto.endDate,
+                    java.util.UUID.randomUUID().toString(),
+                    null,
+                    emptyList<PollItem>().toMutableList()
+                )
+                pollRepository.saveAndFlush(poll)
+                return poll.toDtoOut()
+            }
+    }
+
+
+    //-------------------------------------------- Delete --------------------------------------------------------------
+    fun deletePoll(id: Long) {
+        try {
+            pollRepository.deleteById(id)
+        } catch (ex: EmptyResultDataAccessException) {
+            throw ResponseStatusException(HttpStatus.NO_CONTENT)
         }
     }
 
-    fun updatePoll(pollId: Int, poll: PollDtoIn): ResponseEntity<*> {
-        pollRepository.findById(pollId).orElseGet {
-            throw ResponseStatusException(HttpStatus.NOT_FOUND, "This poll does not exist")
+
+    //-------------------------------------------- Update --------------------------------------------------------------
+    fun updatePoll(pollId: Long, poll: PollDtoIn): PollDtoOut {
+        pollRepository.findById(pollId).orElseThrow {
+            ResponseStatusException(HttpStatus.NOT_FOUND, "This poll does not exist")
         }.run {
             this.name = poll.name
             this.startDate = poll.startDate
-            this.endDate = endDate
-            this.slug = slug
-            if(poll.currentItem != null){
+            this.endDate = poll.endDate
+            this.slug = poll.slug.toString()
+            if (poll.currentItem != null) {
                 this.currentItem = poll.currentItem
             }
-            pollRepository.saveAndFlush(this)
-            return ResponseEntity.ok().body(this)
+            return pollRepository.saveAndFlush(this).toDtoOut()
         }
     }
+
 }
