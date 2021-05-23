a0dfd55a1c395de310f286307abbd4c86e9f87a7
Niklas <65356992+Niklas-23@users.noreply.github.com>
2021-05-23 19:00:34 +0200

    Fix deleting active poll item

--- src/main/kotlin/de/livepoll/api/service/PollItemService.kt
+++ src/main/kotlin/de/livepoll/api/service/PollItemService.kt
@@ -61,6 +61,14 @@ class PollItemService {
     }
 
     fun deleteItem(itemId: Long) {
+        pollItemRepository.findById(itemId).orElseThrow {
+            ResponseStatusException(HttpStatus.NOT_FOUND, "Poll item not found")
+        }.run {
+            val poll = this.poll
+            if (poll.currentItem == itemId) {
+                poll.currentItem = null
+            }
+        }
         pollItemRepository.deleteById(itemId)
     }
 
