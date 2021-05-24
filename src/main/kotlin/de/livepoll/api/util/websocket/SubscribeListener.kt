13bc1583d170f15443b8d39f5c6e3003e7460051
Niklas <65356992+Niklas-23@users.noreply.github.com>
2021-05-24 17:50:02 +0200

    Subscription interceptor for websocket presentation endpoint

--- src/main/kotlin/de/livepoll/api/util/websocket/SubscribeListener.kt
+++ src/main/kotlin/de/livepoll/api/util/websocket/SubscribeListener.kt
@@ -1,7 +1,10 @@
 package de.livepoll.api.util.websocket
 
+import de.livepoll.api.entity.db.PollItem
+import de.livepoll.api.repository.PollItemRepository
 import de.livepoll.api.repository.PollRepository
 import de.livepoll.api.service.PollItemService
+import de.livepoll.api.service.WebSocketService
 import org.springframework.context.ApplicationListener
 import org.springframework.http.HttpStatus
 import org.springframework.messaging.simp.SimpMessageSendingOperations
@@ -12,15 +15,17 @@ import org.springframework.web.socket.messaging.SessionSubscribeEvent
 
 @Component
 class SubscribeListener(
-    private val messagingTemplate: SimpMessageSendingOperations,
-    private val pollRepository: PollRepository,
-    private val pollItemService: PollItemService
+        private val messagingTemplate: SimpMessageSendingOperations,
+        private val pollRepository: PollRepository,
+        private val pollItemService: PollItemService,
+        private val webSocketService: WebSocketService
 ) : ApplicationListener<SessionSubscribeEvent> {
 
     @Transactional
     override fun onApplicationEvent(event: SessionSubscribeEvent) {
-        if( event.message.headers["simpDestination"].toString().contains("poll")){
-            val slug = event.message.headers["simpDestination"].toString().split("/").last()
+        val destination = event.message.headers["simpDestination"].toString()
+        if (destination.contains("poll")) {
+            val slug = destination.split("/").last()
             val poll = pollRepository.findBySlug(slug)
             if (poll == null) {
                 throw ResponseStatusException(HttpStatus.NOT_FOUND)
@@ -32,6 +37,15 @@ class SubscribeListener(
                     val pollItemDto = pollItemService.getPollItem(poll.currentItem!!)
                     messagingTemplate.convertAndSendToUser(event.user!!.name, url, pollItemDto)
                 }
+            }
+        } else if (destination.contains("presentation")) {
+            val pollId = destination.split("/").last().toLong()
+            pollRepository.findById(pollId).orElseThrow {
+                val url = "/v1/websocket/presentation/${pollId}"
+                messagingTemplate.convertAndSendToUser(event.user!!.name, url, "No current item")
+                throw ResponseStatusException(HttpStatus.NOT_FOUND)
+            }.run {
+                webSocketService.sendItemWithAnswers(this.currentItem!!)
             }
         }
     }
