1ce7191b74cbf118a722894debc6abd7957f7fe4
Splines <dominic-plein@gmx.de>
2021-06-10 11:17:23 +0200

    Update allowed CORS origins

--- src/main/kotlin/de/livepoll/api/config/CorsConfig.kt
+++ src/main/kotlin/de/livepoll/api/config/CorsConfig.kt
@@ -12,18 +12,18 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver
 class CorsConfig : WebMvcConfigurer {
 
     private val allowedOrigins = setOf(
-            System.getenv("LIVE_POLL_DEV_URL"),
-            "https://dev.live-poll.de",
-            "https://www.live-poll.de"
+        System.getenv("LIVE_POLL_FRONTEND_URL"),
+        System.getenv("LIVE_POLL_DEV_URL")
     )
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE")
-                .allowedOrigins(*allowedOrigins.toTypedArray())
-                .allowCredentials(true)
+            .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE")
+            .allowedOrigins(*allowedOrigins.toTypedArray())
+            .allowCredentials(true)
     }
 
     @Bean
     fun defaultViewResolver() = InternalResourceViewResolver()
+
 }
